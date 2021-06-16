d04dab373479048f366ffbfdb0395bde3f4baa3a
katrina <katrinadierking@gmail.com>
2021-06-16 18:21:35 -0400

    initial push with cards and lists created with some design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import CardList from './components/card-list/CardList'
+import './App.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [state, setState] = useState({monsters: []})
 
+  useEffect (() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setState({monsters: users}));
+  }, [])
+  
+
+
+  return (
+    <>
+    <div className="App">
+      <CardList monsters = {state.monsters} />
+    </div>
+       
+    </>
+  );
+};
 export default App;
